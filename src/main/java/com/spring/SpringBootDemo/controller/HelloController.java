212043d94919e6ac274562f974c04d111ec74fdf
admin <admin@DESKTOP-FUMQIA5>
2018-12-04 12:05:39 +0800

    spring boot

---
+++ src/main/java/com/spring/SpringBootDemo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.spring.SpringBootDemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping(value="/hello")
+	public String hello() {
+		return "hello";
+		
+	}
+
+}
